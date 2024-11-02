628497d0efd13b24d1a397ffe8e9d3421a9a02a9
Perry102 <141802227+Perry102@users.noreply.github.com>
2024-11-02 19:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    
+    # Initialize the first two terms
+    a, b = 0, 1
+    # Compute up to the nth term
+    for _ in range(2, num + 1):
+        a, b = b, a + b
+    return a
+
 }
 
 module.exports = fibonacci;
