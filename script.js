04450b4a6d3305fdf2d6a127df986a4096ca2c40
Perry102 <141802227+Perry102@users.noreply.github.com>
2024-11-02 19:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function fibonacci(num) {
-// your code here
-    if num == 0:
-        return 0
-    elif num == 1:
-        return 1
-    
-    a, b = 0, 1
-    for _ in range(2, num + 1):
-        a, b = b, a + b
-    return a
+    if (num === 0) return 0;
+    if (num === 1) return 1;
 
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return a;
 }
-
-module.exports = fibonacci;
